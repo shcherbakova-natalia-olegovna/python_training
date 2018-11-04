0f6a6e8f921e27faa415a67b49300424a7194052
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-11-04 22:39:58 +0300

    Исправлен конструктор в group.py

--- group.py
+++ group.py
@@ -1,5 +1,5 @@
 class Group:
-    def _init_(self, name, header, footer):
+    def __init__(self, name, header, footer):
         self.name = name
         self.header = header
         self.footer = footer
