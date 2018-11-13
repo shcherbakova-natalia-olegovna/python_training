3e157901a0e8857e697d414f201a61bfefa0f52d
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-11-14 00:03:31 +0300

    Добавлен тест - "Удаление контакта".

--- fixture/contact.py
+++ fixture/contact.py
@@ -29,7 +29,7 @@ class ContactHelper:
 
     def edit_contact(self, contact):
         wd = self.app.wd
-        wd.find_element_by_id("6").click()
+        wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -48,3 +48,10 @@ class ContactHelper:
         wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
         self.app.return_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
