edac43ec691b0343441a8e4e4651c8b2666c9b2c
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-11-16 17:05:45 +0300

    Повышаем "интеллектуальность" фикстуры.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name="natali", middlename="olegovna", surname="scherbakova", nick="nick", title="title"))
-    app.session.logout()
 
 
