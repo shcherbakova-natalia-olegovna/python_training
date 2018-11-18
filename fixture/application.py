1d0f0d1b4cffec4b8294fc7449225aef581b625e
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-11-18 11:55:17 +0300

    Оптимизированы переходы между страницами.

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,7 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(10)
+        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -22,11 +22,13 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.get("http://localhost/addressbook/")
 
     def return_to_home_page(self):
         wd = self.wd
-        wd.find_element_by_id("logo").click()
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_id("logo").click()
 
     def destroy(self):
         self.wd.quit()
