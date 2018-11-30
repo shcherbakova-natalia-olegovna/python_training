0f55f39e10ea0e2cae996e226a3a7a5a937b92ba
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-11-30 22:16:03 +0300

    Параметризованы тесты для создания групп и контактов.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(name="", middlename="", surname="", nick="", title="")] + [
+    Contact(name=random_string("name", 10), middlename=random_string("middlename", 10),
+          surname=random_string("surname", 10), nick=random_string("nick", 10), title=random_string("title", 10))
+    for i in range(5)
+    ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(name="natali", middlename="olegovna", surname="scherbakova", nick="nick", title="title")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
