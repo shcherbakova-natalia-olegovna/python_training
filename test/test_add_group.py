c977d525c6f970776e4e22762f34b85d72265545
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-12-03 00:07:17 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,7 +7,7 @@ import string
 
 def random_string(prefix, maxlen):
     #symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    symbols = string.ascii_letters + string.digits + " " * 10
+    symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
