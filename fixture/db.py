1499a46a03c6dc77fc439220d892beba635185aa
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2018-12-16 22:45:04 +0300

    Загрузка информации из базы данных в виде объектов(1).

---
+++ fixture/db.py
@@ -0,0 +1,21 @@
+import pymysql.cursors
+
+class DbFixture:
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+        finally:
+            cursor.close()
+
+    def destroy(self):
+        self.connection.close()
