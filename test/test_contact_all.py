dd6b0dc3676274f58ce1c8961227f70d1f92ac99
shcherbakova-natalia-olegovna <NataliaShcherbakovaAscon@gmail.com>
2019-01-07 14:17:05 +0300

    Задание 21(исправлено).

--- test/test_contact_all.py
+++ test/test_contact_all.py
@@ -1,17 +1,28 @@
 import re
-from random import randrange
+#from random import randrange
+from model.contact import Contact
 
 
-def test_contact_all(app):
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.surname == contact_from_edit_page.surname
-    assert contact_from_home_page.name == contact_from_edit_page.name
-    assert clear_probel(contact_from_home_page.address) == clear_probel(contact_from_edit_page.address)
-    assert clear_probel(contact_from_home_page.all_emails_from_home_page) == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+def test_contact_all(app, db):
+    ui_list = app.contact.get_contact_list()
+
+    def clean(contact):
+        return Contact(id=contact.id, name=contact.name.strip(), surname=contact.surname.strip(), address=contact.address,
+                       all_emails_from_home_page=merge_emails_like_on_home_page(contact), all_phones_from_home_page=merge_phones_like_on_home_page(contact))
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
+
+
+#def test_contact_all(app):
+    #old_contacts = app.contact.get_contact_list()
+    #index = randrange(len(old_contacts))
+    #contact_from_home_page = app.contact.get_contact_list()[index]
+    #contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    #assert contact_from_home_page.surname == contact_from_edit_page.surname
+    #assert contact_from_home_page.name == contact_from_edit_page.name
+    #assert clear_probel(contact_from_home_page.address) == clear_probel(contact_from_edit_page.address)
+    #assert clear_probel(contact_from_home_page.all_emails_from_home_page) == merge_emails_like_on_home_page(contact_from_edit_page)
+    #assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def clear(s):
